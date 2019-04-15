284cd434d364a431cb2e953c21404e5ce1ff277b
Sébastien Gardoll <sebastien.gardoll@ipsl.fr>
2019-04-15 16:09:57 +0200

    add prototype of the class Passeur

---
+++ src/fr/gardoll/ace/controller/sampler/Passeur.java
@@ -0,0 +1,22 @@
+package fr.gardoll.ace.controller.sampler;
+
+import java.io.Closeable ;
+import java.io.IOException ;
+
+public class Passeur implements Closeable
+{
+
+  public static void main(String[] args)
+  {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void close() throws IOException
+  {
+    // TODO Auto-generated method stub
+    
+  }
+
+}
