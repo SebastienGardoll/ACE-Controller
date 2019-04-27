60eae2dea99cd49799bf8410cb4ab05f5f1fd6f7
Sébastien Gardoll <sebastien@gardoll.fr>
2019-04-27 12:02:06 +0200

    imple root dir computation

--- src/fr/gardoll/ace/controller/common/Utils.java
+++ src/fr/gardoll/ace/controller/common/Utils.java
@@ -1,5 +1,9 @@
 package fr.gardoll.ace.controller.common;
 
+import java.net.URISyntaxException ;
+import java.net.URL ;
+import java.nio.file.Path ;
+
 public class Utils
 {
   private Utils() {}
@@ -9,6 +13,15 @@ public class Utils
   public static boolean isNearZero(double value)
   {
     return (value <= EPSILON);
+  }
+  
+  // Return the path of the directory of the application (not the current
+  // directory !).
+  public static Path getRootDir(Object obj) throws URISyntaxException
+  {
+    URL u = obj.getClass().getProtectionDomain().getCodeSource().getLocation();
+    Path result = Path.of(u.toURI()).getParent();
+    return result;
   }
   
 }
