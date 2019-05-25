9f798b0237da9a669d88aa9854817ad5bc24996a
Sébastien Gardoll <sebastien@gardoll.fr>
2019-05-25 18:50:09 +0200

    create abstract frame class

---
+++ src/fr/gardoll/ace/controller/ui/AbstractJFrame.java
@@ -0,0 +1,45 @@
+package fr.gardoll.ace.controller.ui;
+
+import java.awt.event.WindowAdapter ;
+import java.awt.event.WindowEvent ;
+
+import javax.swing.JFrame ;
+import javax.swing.JOptionPane ;
+import javax.swing.JPanel ;
+import javax.swing.WindowConstants ;
+
+import org.apache.logging.log4j.LogManager ;
+import org.apache.logging.log4j.Logger ;
+
+public abstract class AbstractJFrame extends JFrame
+{
+  private static final Logger _LOG = LogManager.getLogger(AbstractJFrame.class.getName());
+  
+  private static final long serialVersionUID = -2440934563391351769L ;
+  
+  public AbstractJFrame(JPanel mainPanel)
+  {
+    this.getContentPane().add(mainPanel);
+    
+    this.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+    this.addWindowListener(new WindowAdapter()
+    {
+      @Override
+      public void windowClosing(WindowEvent e)
+      {
+        int choice = JOptionPane.showConfirmDialog(AbstractJFrame.this,
+            "Do you want to exit (eventually cancel the running operations) ?") ;
+        if (choice == JOptionPane.OK_OPTION)
+        {
+          _LOG.debug("running the close operations");
+          AbstractJFrame.this.onCloseOperation();
+          _LOG.debug("closing the main frame and shutting down the JVM");
+          AbstractJFrame.this.dispose();
+        }
+      }
+    }) ;
+  }
+  
+  protected abstract void onCloseOperation();
+
+}
