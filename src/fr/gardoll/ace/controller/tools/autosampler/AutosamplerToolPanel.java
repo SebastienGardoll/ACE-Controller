587aadfab1421e9d619b8be43604a1df79be88dc
Sébastien Gardoll <sebastien.gardoll@ipsl.fr>
2019-06-13 14:17:24 +0200

    disable pause and cancel button at start

--- src/fr/gardoll/ace/controller/tools/autosampler/AutosamplerToolPanel.java
+++ src/fr/gardoll/ace/controller/tools/autosampler/AutosamplerToolPanel.java
@@ -33,8 +33,8 @@ public class AutosamplerToolPanel extends AbstractJPanelObserver
   {
     super(ctrl);
     this._ctrl = ctrl;
-    initCustom();
     initComponents() ;
+    initCustom();
   }
 
   /**
@@ -760,6 +760,8 @@ public class AutosamplerToolPanel extends AbstractJPanelObserver
         return String.format("column file *.%s", Colonne.COLUMN_FILE_EXTENTION);
       }
     });
+    
+    this.enableControl(true);
   }
   
   private void addToUi(String msg)
