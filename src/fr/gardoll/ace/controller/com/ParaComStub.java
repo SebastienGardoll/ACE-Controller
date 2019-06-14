04dc132fcb2dd2bb48f81444b5875ada73fa04eb
Sébastien Gardoll <sebastien.gardoll@ipsl.fr>
2019-06-14 15:52:13 +0200

    impl paracom stub

---
+++ src/fr/gardoll/ace/controller/com/ParaComStub.java
@@ -0,0 +1,42 @@
+package fr.gardoll.ace.controller.com;
+
+import org.apache.logging.log4j.LogManager ;
+import org.apache.logging.log4j.Logger ;
+
+import fr.gardoll.ace.controller.core.ParaComException ;
+
+public class ParaComStub implements ParaCom
+{
+  private static final Logger _LOG = LogManager.getLogger(ParaComStub.class.getName());
+  
+  @Override
+  public String getId()
+  {
+    _LOG.debug("stubbing method getId");
+    return "stub";
+  }
+
+  @Override
+  public void ouvrir(int numEv) throws ParaComException, InterruptedException
+  {
+    _LOG.debug(String.format("stubbing openning valve %s", numEv));
+  }
+
+  @Override
+  public void ouvrirH2O() throws ParaComException, InterruptedException
+  {
+    _LOG.debug("stubbing openning H20 valve");
+  }
+
+  @Override
+  public void toutFermer() throws ParaComException, InterruptedException
+  {
+    _LOG.debug("stubbing closing all valves");
+  }
+
+  @Override
+  public void close()
+  {
+    _LOG.debug("stubbing closing paracom");
+  }
+}
