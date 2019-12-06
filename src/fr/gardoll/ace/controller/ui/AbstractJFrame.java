5d142255465e5a0870b119e2d58dbf345d63579f
Sébastien Gardoll <sebastien@gardoll.fr>
2019-12-06 18:10:15 +0100

    disable closing window

--- src/fr/gardoll/ace/controller/ui/AbstractJFrame.java
+++ src/fr/gardoll/ace/controller/ui/AbstractJFrame.java
@@ -1,36 +1,20 @@
 package fr.gardoll.ace.controller.ui;
 
-import java.awt.event.WindowAdapter ;
-import java.awt.event.WindowEvent ;
-
 import javax.swing.JDialog ;
+import javax.swing.JPanel ;
 import javax.swing.WindowConstants ;
-
-import org.apache.logging.log4j.LogManager ;
-import org.apache.logging.log4j.Logger ;
 
 public abstract class AbstractJFrame extends JDialog
 {
   private static final long serialVersionUID = -2440934563391351769L ;
   
-  private static final Logger _LOG = LogManager.getLogger(AbstractJFrame.class.getName());
-  
-  public AbstractJFrame(AbstractJPanelObserver mainPanel)
+  public AbstractJFrame(JPanel mainPanel)
   {
     super(null, ModalityType.APPLICATION_MODAL);
     this.getContentPane().add(mainPanel);
     
     // Manage the closing operations.
     this.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
-    this.addWindowListener(new WindowAdapter()
-    {
-      @Override
-      public void windowClosing(WindowEvent e)
-      {
-        _LOG.debug("**** windowClosing *****");
-        mainPanel.close();
-      }
-    }) ;
     this.pack();
     this.setLocationRelativeTo(null) ;
   }
