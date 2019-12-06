64bb50ac5df6c8246469e17fad79f5e5f47d1ea7
Sébastien Gardoll <sebastien@gardoll.fr>
2019-12-06 17:15:53 +0100

    add valves tool tests

---
+++ src/fr/gardoll/ace/controller/tools/valves/ValvesToolTest.java
@@ -0,0 +1,70 @@
+package fr.gardoll.ace.controller.tools.valves;
+
+import org.apache.logging.log4j.LogManager ;
+import org.apache.logging.log4j.Logger ;
+import org.junit.jupiter.api.AfterEach ;
+import org.junit.jupiter.api.BeforeAll ;
+import org.junit.jupiter.api.BeforeEach ;
+import org.junit.jupiter.api.Test ;
+
+import fr.gardoll.ace.controller.core.ParametresSession ;
+import fr.gardoll.ace.controller.ui.CloseableJPanelObserverStub ;
+
+class ValvesToolTest
+{
+  private static final Logger _LOG = LogManager.getLogger(ValvesToolTest.class.getName());
+  
+  ValvesToolControl _ctrl = null;
+  CloseableJPanelObserverStub _toolPanel = null;
+  
+  @BeforeAll
+  static void setUpBeforeClass() throws Exception
+  {
+    ParametresSession.isAutomatedTest = true;
+  }
+
+  @BeforeEach
+  void setUp() throws Exception
+  {
+    _LOG.info("******************** setup");
+    ParametresSession parametresSession = ParametresSession.getInstance();
+    this._ctrl = new ValvesToolControl(parametresSession);
+    this._toolPanel = new CloseableJPanelObserverStub(this._ctrl);
+    this._ctrl.addControlPanel(this._toolPanel);
+  }
+
+  @AfterEach
+  void tearDown() throws Exception
+  {
+    _LOG.info("******************** teardown");
+    this._ctrl.close();
+    ParametresSession.getInstance().close();
+  }
+
+  @Test
+  void scriptedTest1() throws InterruptedException
+  {
+    _LOG.info("******************** scripted test1");
+    _LOG.info("* open sequentially every valve *");
+    for(int valveId=0 ; valveId < 8 ; valveId++)
+    {
+      this._ctrl.handleValve(valveId);
+      Thread.sleep(1000);
+    }
+  }
+  
+  @Test
+  void scriptedTest2() throws InterruptedException
+  {
+    _LOG.info("******************** scripted test2");
+    _LOG.info("* open and shut sequentially every valve *");
+    for(int valveId=1 ; valveId < 8 ; valveId++)
+    {
+      this._ctrl.handleValve(valveId);
+      Thread.sleep(1000);
+      this._ctrl.handleValve(valveId);
+      Thread.sleep(1000);
+    }
+  }
+
+}
---
+++ src/fr/gardoll/ace/controller/ui/CloseableJPanelObserverStub.java
@@ -0,0 +1,36 @@
+package fr.gardoll.ace.controller.ui;
+
+import org.apache.logging.log4j.LogManager ;
+import org.apache.logging.log4j.Logger ;
+
+import fr.gardoll.ace.controller.core.ToolControl ;
+
+public class CloseableJPanelObserverStub extends AbstractCloseableJPanelObserver
+{
+  private static final Logger _LOG = LogManager.getLogger(CloseableJPanelObserverStub.class.getName());
+  
+  public CloseableJPanelObserverStub(ToolControl ctrl)
+  {
+    super(ctrl) ;
+  }
+
+  private static final long serialVersionUID = 3868647314433761655L ;
+
+  @Override
+  protected void displayToUserLogSys(String msg)
+  {
+    _LOG.info(String.format("display '%s'", msg.strip()));
+  }
+
+  @Override
+  protected void enableCloseControl(boolean isEnable)
+  {
+    _LOG.trace(String.format("close control set to %s", isEnable));
+  }
+  
+  @Override
+  public void dispose()
+  {
+    // Nothing to do.
+  }
+}
