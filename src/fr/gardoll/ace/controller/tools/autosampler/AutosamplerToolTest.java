6b4a367b4b9981edcdc3673f7c27662dce56843a
Sébastien Gardoll <sebastien@gardoll.fr>
2019-12-06 15:51:42 +0100

    remove useless method

--- src/fr/gardoll/ace/controller/tools/autosampler/AutosamplerToolTest.java
+++ src/fr/gardoll/ace/controller/tools/autosampler/AutosamplerToolTest.java
@@ -4,7 +4,6 @@ import java.nio.file.Path ;
 
 import org.apache.logging.log4j.LogManager ;
 import org.apache.logging.log4j.Logger ;
-import org.junit.jupiter.api.AfterAll ;
 import org.junit.jupiter.api.AfterEach ;
 import org.junit.jupiter.api.BeforeAll ;
 import org.junit.jupiter.api.BeforeEach ;
@@ -26,11 +25,6 @@ class AutosamplerToolTest
   static void setUpBeforeClass() throws Exception
   {
     ParametresSession.isAutomatedTest = true;
-  }
-
-  @AfterAll
-  static void tearDownAfterClass() throws Exception
-  {
   }
 
   @BeforeEach
