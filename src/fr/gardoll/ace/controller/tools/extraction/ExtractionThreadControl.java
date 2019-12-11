95885cf0ce30fcc7ff46fc744680b16be64504f6
Sébastien Gardoll <sebastien@gardoll.fr>
2019-12-11 16:28:45 +0100

    add the skeleton of extraction tool control

---
+++ src/fr/gardoll/ace/controller/tools/extraction/ExtractionThreadControl.java
@@ -0,0 +1,26 @@
+package fr.gardoll.ace.controller.tools.extraction;
+
+import org.apache.logging.log4j.LogManager ;
+import org.apache.logging.log4j.Logger ;
+
+import fr.gardoll.ace.controller.core.AbstractThreadControl ;
+import fr.gardoll.ace.controller.core.AbstractToolControl ;
+import fr.gardoll.ace.controller.core.CancellationException ;
+import fr.gardoll.ace.controller.core.InitializationException ;
+
+public class ExtractionThreadControl extends AbstractThreadControl
+{
+  public ExtractionThreadControl(AbstractToolControl toolCtrl)
+  {
+    super(toolCtrl) ;
+  }
+
+  private static final Logger _LOG = LogManager.getLogger(ExtractionThreadControl.class.getName());
+
+  @Override
+  protected void threadLogic() throws InterruptedException,
+      CancellationException, InitializationException, Exception
+  {
+    
+  }
+}
