8748af40a8159e17d64d69a5a9d1c1820e4bd8a4
Sébastien Gardoll <sebastien@gardoll.fr>
2019-12-15 13:02:51 +0100

    externalyze ControlPanelAdapter

---
+++ src/fr/gardoll/ace/controller/core/ControlPanelAdapter.java
@@ -0,0 +1,71 @@
+package fr.gardoll.ace.controller.core;
+
+public abstract class ControlPanelAdapter implements ControlPanel
+{
+  @Override
+  public void reportError(String msg, Throwable e)
+  {
+    // Nothing to do.
+  }
+
+  @Override
+  public void reportError(String msg)
+  {
+    // Nothing to do.  
+  }
+
+  @Override
+  public void displayModalMessage(String msg)
+  {
+    // Nothing to do.
+  }
+
+  @Override
+  public boolean close()
+  {
+    // Nothing to do.
+    return true;
+  }
+
+  @Override
+  public void dispose()
+  {
+    // Nothing to do.
+  }
+
+  @Override
+  public void enableStart(boolean isEnable)
+  {
+    // Nothing to do.
+  }
+
+  @Override
+  public void enableClose(boolean isEnable)
+  {
+    // Nothing to do.
+  }
+
+  @Override
+  public void enablePause(boolean isEnable)
+  {
+    // Nothing to do.
+  }
+
+  @Override
+  public void enableResume(boolean isEnable)
+  {
+    // Nothing to do.
+  }
+
+  @Override
+  public void enableCancel(boolean isEnable)
+  {
+    // Nothing to do.
+  }
+
+  @Override
+  public void enableReinit(boolean isEnable)
+  {
+    // Nothing to do.
+  }
+}
--- src/fr/gardoll/ace/controller/tools/pump/PumpToolTest.java
+++ src/fr/gardoll/ace/controller/tools/pump/PumpToolTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test ;
 
 import fr.gardoll.ace.controller.core.Action ;
 import fr.gardoll.ace.controller.core.ControlPanel ;
+import fr.gardoll.ace.controller.core.ControlPanelAdapter ;
 import fr.gardoll.ace.controller.core.ParametresSession ;
 import fr.gardoll.ace.controller.ui.PausableJPanelObserverStub ;
 
@@ -256,75 +257,5 @@ class PumpToolTest
     lines.add(1);
     this._ctrl.start(lines, 5);
     PumpToolTest.this._toolPanel.waitMove();
-  }
-  
-  abstract class ControlPanelAdapter implements ControlPanel
-  {
-    @Override
-    public void reportError(String msg, Throwable e)
-    {
-      // Nothing to do.
-    }
-
-    @Override
-    public void reportError(String msg)
-    {
-      // Nothing to do.  
-    }
-
-    @Override
-    public void displayModalMessage(String msg)
-    {
-      // Nothing to do.
-    }
-
-    @Override
-    public boolean close()
-    {
-      // Nothing to do.
-      return true;
-    }
-
-    @Override
-    public void dispose()
-    {
-      // Nothing to do.
-    }
-
-    @Override
-    public void enableStart(boolean isEnable)
-    {
-      // Nothing to do.
-    }
-
-    @Override
-    public void enableClose(boolean isEnable)
-    {
-      // Nothing to do.
-    }
-
-    @Override
-    public void enablePause(boolean isEnable)
-    {
-      // Nothing to do.
-    }
-
-    @Override
-    public void enableResume(boolean isEnable)
-    {
-      // Nothing to do.
-    }
-
-    @Override
-    public void enableCancel(boolean isEnable)
-    {
-      // Nothing to do.
-    }
-
-    @Override
-    public void enableReinit(boolean isEnable)
-    {
-      // Nothing to do.
-    }
   }
 }
