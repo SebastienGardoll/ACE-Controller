3272556b416d3298fa8e12af45953a1773011b8e
Sébastien Gardoll <sebastien@gardoll.fr>
2019-12-23 17:57:18 +0100

    impl main tool

---
+++ src/fr/gardoll/ace/controller/tools/main/MainToolFrame.java
@@ -0,0 +1,48 @@
+package fr.gardoll.ace.controller.tools.main;
+
+import javax.swing.JPanel ;
+
+import org.apache.logging.log4j.LogManager ;
+import org.apache.logging.log4j.Logger ;
+
+import fr.gardoll.ace.controller.core.InitializationException ;
+import fr.gardoll.ace.controller.core.ParametresSession ;
+import fr.gardoll.ace.controller.core.Utils ;
+import fr.gardoll.ace.controller.ui.AbstractToolFrame ;
+
+public class MainToolFrame extends AbstractToolFrame
+{
+  private static final long serialVersionUID = 3741510571374337328L ;
+  private static final Logger _LOG = LogManager.getLogger(MainToolFrame.class.getName());
+  
+  public MainToolFrame(JPanel mainPanel)
+  {
+    super(mainPanel) ;
+  }
+
+  public static void main(String[] args)
+  {
+    try(ParametresSession parametresSession = ParametresSession.getInstance())
+    {
+      MainToolPanel mainPanel = new MainToolPanel(parametresSession);
+      MainToolFrame tool = new MainToolFrame(mainPanel);
+      tool.setVisible(true);
+    }
+    catch (Exception e)
+    {
+      String msg = null;
+      
+      if(e.getCause() instanceof InitializationException)
+      {
+        msg = "initialisation has crashed";
+      }
+      else
+      {
+        msg = "tool has crashed";
+      }
+      
+      _LOG.fatal(msg, e);
+      Utils.reportError(msg, e);
+    }
+  }
+}
