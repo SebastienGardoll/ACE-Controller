33dc9428346f0088acfcb7deeb1a14540a85a8d7
Sébastien Gardoll <sebastien@gardoll.fr>
2020-01-08 14:44:51 +0100

    add settings gui

---
+++ src/fr/gardoll/ace/controller/settings/Panel.java
@@ -0,0 +1,9 @@
+package fr.gardoll.ace.controller.settings;
+
+import fr.gardoll.ace.controller.core.ConfigurationException ;
+
+public interface Panel
+{
+  public void save() throws ConfigurationException;
+  public String getPanelName();
+}
---
+++ src/fr/gardoll/ace/controller/settings/SettingsFrame.java
@@ -0,0 +1,57 @@
+package fr.gardoll.ace.controller.settings;
+
+import javax.swing.JPanel ;
+
+import org.apache.logging.log4j.LogManager ;
+import org.apache.logging.log4j.Logger ;
+
+import fr.gardoll.ace.controller.core.InitializationException ;
+import fr.gardoll.ace.controller.core.Utils ;
+import fr.gardoll.ace.controller.ui.AbstractToolFrame ;
+
+public class SettingsFrame extends AbstractToolFrame
+{
+  private static final Logger _LOG = LogManager.getLogger(SettingsFrame.class.getName());
+  private static final long serialVersionUID = -5959896878773369661L ;
+
+  public SettingsFrame(JPanel mainPanel)
+  {
+    super(mainPanel) ;
+  }
+  
+  public static void main(String[] args)
+  {
+    _LOG.info("start the tool");
+    
+    try
+    {
+      SettingsFrame settings = SettingsFrame.instantiate();
+      settings.setVisible(true);
+    }
+    catch (Exception e)
+    {
+      String msg = null;
+      
+      if(e.getCause() instanceof InitializationException)
+      {
+        msg = "intialisation has crashed";
+      }
+      else
+      {
+        msg = "settings has crashed";
+      }
+      
+      _LOG.fatal(msg, e);
+      Utils.reportError(msg, e);
+    }
+    finally
+    {
+      _LOG.info("shutdown the settings");
+    }
+  }
+
+  public static SettingsFrame instantiate()
+  {
+    return new SettingsFrame(new SettingsPanel());
+  }
+}
