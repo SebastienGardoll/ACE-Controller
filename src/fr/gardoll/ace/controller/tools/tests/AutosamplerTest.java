c6df0134022f2e6aec636af4718ad1afb186e8d1
Sébastien Gardoll <sebastien@gardoll.fr>
2020-01-18 14:11:33 +0100

    impl autosampler test logic

--- src/fr/gardoll/ace/controller/tools/tests/AutosamplerTest.java
+++ src/fr/gardoll/ace/controller/tools/tests/AutosamplerTest.java
@@ -27,16 +27,81 @@ public class AutosamplerTest extends AsbtractTest
   protected List<Operation> createOperations(Passeur autosampler,
       PousseSeringue pump, Valves valves)
   {
-    
+    long delay = 5000l;
     List<Operation> operations = new ArrayList<>();
     
-    operations.add(new Operation("- to trash", ()-> {
-      autosampler.moveArmToTrash();
-      autosampler.finMoveBras() ;
+    operations.add(new Operation("- referencing (nothing should happen)", ()-> {
+      autosampler.setOrigineBras();
+      Thread.sleep(delay);
     }));
     
+    operations.add(new Operation("- arm goes down of 50 mm", ()-> {
+      int nbStep = Passeur.convertBras(-50.);
+      autosampler.moveBras(nbStep);
+      autosampler.finMoveBras() ;
+      autosampler.setOrigineBras();
+      Thread.sleep(delay);
+    }));
+    
+    operations.add(new Operation("- arm goes up of 25 mm", () -> {
+      int nbStep = Passeur.convertBras(25.);
+      autosampler.moveBras(nbStep);
+      autosampler.finMoveBras() ;
+      Thread.sleep(delay);
+    })) ;
+
+    operations.add(new Operation("- arm returns to the last position", () -> {
+      autosampler.moveOrigineBras();
+      autosampler.finMoveBras() ;
+      Thread.sleep(delay);
+    })) ;
+    
+    operations.add(new Operation("- arm goes to the top", ()-> {
+      autosampler.moveButeBras();
+      autosampler.finMoveBras() ;
+      autosampler.setOrigineBras();
+      Thread.sleep(delay);
+    }));
+    
+    operations.add(new Operation("- arm goes to trash", ()-> {
+      autosampler.moveArmToTrash();
+      autosampler.finMoveBras() ;
+      Thread.sleep(delay);
+    }));
+    
+    operations.add(new Operation("- arm goes to the top", ()-> {
+      autosampler.moveButeBras();
+      autosampler.finMoveBras() ;
+      autosampler.setOrigineBras();
+      Thread.sleep(delay);
+    }));
+    
+    operations.add(new Operation("- referencing carousel (nothing should do)", () -> {
+      autosampler.setOrigineCarrousel();
+      Thread.sleep(delay);
+    })) ;
+    
+    operations.add(new Operation("- carousel turns 6 positions to the LEFT", ()-> {
+      autosampler.moveCarrousel(-6);
+      autosampler.finMoveCarrousel();
+      Thread.sleep(delay);
+    }));
+    
+    operations.add(new Operation("- carousel turns 3 positions to the RIGHT", ()-> {
+      autosampler.moveCarrousel(3);
+      autosampler.finMoveCarrousel();
+      Thread.sleep(delay);
+    }));
+    
+    operations.add(new Operation("- carousel returns to the trash (3 positions to the RIGHT)", () -> {
+      autosampler.moveOrigineCarrousel();
+      autosampler.finMoveCarrousel();
+      Thread.sleep(delay);
+    })) ;
+
     operations.add(new Operation("- reinit", ()-> {
       autosampler.reinit();
+      Thread.sleep(delay);
     }));
     
     return operations;
